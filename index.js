00640f9487f103415c1c3af450d751332119cabc
Colin Daniels <colindaniels@Colins-MacBook-Pro-2.local>
2022-06-04 10:24:38 -0500

    stable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+require('dotenv').config();
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -9,56 +10,81 @@ const cheerio = require('cheerio');
 
     await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36')
 
-    await page.goto('https://twitter.com/benshapiro', { waitUntil: 'networkidle2' });
+    await page.goto('https://twitter.com/search?q=from%3Abenshapiro%20since%3A2006-03-21&src=typed_query&f=live', { waitUntil: 'networkidle2' });
     await page.setViewport({ width: 1280, height: 800 });
+
 
     await page.waitForSelector('article')
 
-    var tweets_elements = []
 
     var tweets_obj = {
         tweets: []
     }
+    var tweet_ids = []
 
     const total_tweets = 100
 
-    async function getTweetsAndScroll() {
+    async function getTweets() {
         let bodyHTML = await page.evaluate(() => document.body.innerHTML);
         let $ = cheerio.load(bodyHTML)
-        let all_tweets = $(bodyHTML).find('article[data-testid="tweet"]')
-        all_tweets.each(function() {
-            if (!tweets_elements.includes($(this).find('time').attr('datetime')) && tweets_obj.tweets.length < total_tweets) {
-                let tweet = $(this).find('[data-testid="tweetText"]').eq(0).text()
+        let all_tweets = $(bodyHTML).find('[data-testid="primaryColumn"] section > div > div > div')
+        let tweets = []
+        all_tweets.each(function () {
+            // don't allow duplicates
+            let id = Number($(this).attr('style').split(' ').at(1).replace('translateY(', '').replace('px);', ''))
+            if (!tweet_ids.includes(id) && tweets_obj.tweets.length + tweets.length < total_tweets) {
+                let text = $(this).find('[data-testid="tweetText"]').eq(0).text()
                 let date = $(this).find('time').attr('datetime')
-                tweets_obj.tweets.push({
-                    tweet,
-                    date
-                })
-                tweets_elements.push($(this).find('time').attr('datetime'))
+                metadata = {
+                    text,
+                    date,
+                    id
+                }
+                tweets.push(metadata)
             }
         })
-
-
-        var currentStyle = ''
-        await page.evaluate(() => {
-            document.querySelector('nav ~ section > div > div > div:last-child').scrollIntoView()
-            currentStyle = Number(document.querySelector('nav ~ section > div > div').getAttribute('style').split(' ').at(-1).replace('px;', ''))
-            console.log(currentStyle)
+        return tweets
+    }
+    function getTweetsAndScroll() {
+        console.log('i')
+        getTweets().then(async (tweets) => {
+            tweets.forEach((t) => {
+                // what user sees
+                tweets_obj.tweets.push({
+                    text: t.text,
+                    date: t.date
+                })
+                // add to exclusion array
+                tweet_ids.push(t.id)
+            })
+            if (tweets_obj.tweets.length < total_tweets) {
+                var currentHTML = ''
+                await page.evaluate((ids) => {
+                    document.querySelector('[data-testid="primaryColumn"] section > div > div > div:last-child').scrollIntoView()
+                    currentHTML = document.querySelector('[data-testid="primaryColumn"] section > div > div').innerHTML
+                }, tweet_ids)
+                await page.waitForFunction(`document.querySelector('[data-testid="primaryColumn"] section > div > div').innerHTML != currentHTML`)
+                await page.waitForFunction(`Array.from(document.querySelectorAll('[data-testid="primaryColumn"] section > div > div > div')).map(e => Number(e.getAttribute('style').split(' ').at(1).replace('translateY(', '').replace('px);', ''))).includes(${tweet_ids.at(-1)})`)
+                await getTweetsAndScroll()
+            }
+            else {
+                console.log(tweets_obj)
+                console.log(tweets_obj.tweets.length)
+                console.log(tweet_ids)
+            }
         })
-        await page.evaluate(() => { console.log(Number(document.querySelector('nav ~ section > div > div').getAttribute('style').split(' ').at(-1).replace('px;', ''))) })
-        await page.waitForFunction("Number(document.querySelector('nav ~ section > div > div').getAttribute('style').split(' ').at(-1).replace('px;', '')) != currentStyle")
-        console.log('waited')
-        //await page.waitForTimeout(3000)
-        if (tweets_obj.tweets.length < total_tweets) {
-            getTweetsAndScroll()
-        }
-        else {
-            console.log(tweets_obj)
-            console.log(tweets_obj.tweets.length)
-        }
     }
     getTweetsAndScroll()
 
+    /*
+
+    //
+
+let lst = []
+document.querySelectorAll('[data-testid="primaryColumn"] section > div > div > div').forEach((e) => { lst.push(Number(e.getAttribute('style').split(' ').at(1).replace('translateY(', '').replace('px);', ''))) })
+console.log(lst)
+
+*/
 
 
 
