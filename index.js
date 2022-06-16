6a4776552609b1684c9395d08bfecc51115a26c7
Colin Daniels <colindaniels@colins-mbp-2.nandor.net>
2022-06-16 00:03:10 -0500

    production ready

--- index.js
+++ index.js
@@ -2,211 +2,125 @@ const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
+const cliProgress = require('cli-progress');
+const colors = require('ansi-colors');
+const fs = require('fs');
+process.setMaxListeners(0);
+
+
 require('dotenv').config();
 
-// Pass `handle` as a commandline
-const argv = process.argv.slice(2)
-
-console.log(argv)
-
-var handle = argv[0];
-var total_tweets = argv[1];
-// const handle = 'barackobama';
-// const total_tweets = 10;
-
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: true
-    })
-    const page = await browser.newPage()
-    await page.setViewport({ width: 1280, height: 10000 });
-
-    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36')
-
-    var i = 1
-    var big_list = []
-    await page.on('response', async (r) => {
-        if (r.url().startsWith('https://twitter.com/i/api/2/search/adaptive.json')) {
 
 
+const argv = process.argv.slice(2);
+const handles_file = argv[0];
+const total_tweets = argv[1];
 
-            async function get_tweets_on_page() {
+const handles_list = fs.readFileSync(handles_file, "utf8").split('\n').slice(1)
+
+const bar1 = new cliProgress.SingleBar({
+    format: 'Progress |' + colors.green('{bar}') + '| {percentage}% || {value}/{total} Expected Tweets',
+    barCompleteChar: '\u2588',
+    barIncompleteChar: '\u2591'
+});
+bar1.start(total_tweets * handles_list.length, 0);
+
+
+function getTweetsFromHandle(handle) {
+    return new Promise(async (resolve, reject) => {
+        let i = 1
+        let user_id = ''
+        let big_list = []
+        let browser = await puppeteer.launch({
+            headless: true
+        })
+        let page = await browser.newPage()
+        await page.setViewport({ width: 1280, height: 1000 });
+        await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36')
+
+
+        page.on('response', async (r) => {
+            if (r.url().startsWith('https://twitter.com/i/api/2/search/adaptive.json')) {
                 let response = await r.json()
-                let tweets_array = []
-                for (let id of Object.keys(response.globalObjects.tweets)) {
-                    let tweet = response.globalObjects.tweets[id]
-                    tweets_array.push({
-                        text: tweet.full_text,
-                        date: tweet.created_at
+                if (Object.keys(response.globalObjects.tweets).length !== 0) {
+                    let tweets_array = []
+                    for (let id of Object.keys(response.globalObjects.tweets)) {
+                        let tweet = response.globalObjects.tweets[id]
+                        tweets_array.push({
+                            text: tweet.full_text,
+                            date: tweet.created_at,
+                            user_id: tweet.user_id_str
+                        })
+
+                    }
+                    tweets_array.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+
+                    if (i == 1) {
+                        user_id = tweets_array[0].user_id
+                    }
+
+
+                    tweets_array.forEach(async (t) => {
+                        if (i <= total_tweets) {
+                            if (t.user_id == user_id) {
+                                big_list.push(t)
+                                bar1.increment()
+
+                                i++
+                            }
+
+
+                        }
+                        else {
+                            resolve(big_list)
+                        }
+
                     })
                 }
-                tweets_array.forEach((t) => {
-                    if (i <= total_tweets) {
-                        big_list.push(t)
-                        console.log(i)
-                        i++
-                    }
-
-
-                })
-
-                if (i <= total_tweets) {
-                    await page.waitForSelector('[data-testid="primaryColumn"] section > div > div')
-                    await page.evaluate(() => {
-                        window.scrollTo(0, document.body.scrollHeight);
-                    })
-
-                }
                 else {
-                    console.log('done')
-                    let sorted_by_date_tweets = big_list.sort((a, b) => new Date(b.date) - new Date(a.date));
-                    console.log(sorted_by_date_tweets)
-                    console.log(sorted_by_date_tweets.length)
-                    process.exit()
+                    resolve(big_list)
                 }
-            }
 
 
 
 
-
-
-
-
-
-            get_tweets_on_page()
-        }
-
-    })
-    await page.goto(`https://twitter.com/search?q=from%3A${handle}%20since%3A2006-03-21&src=typed_query&f=live`, { waitUntil: 'networkidle2' });
-
-
-    /*
-
-    await page.waitForSelector('article')
-
-
-    var tweets_obj = {
-        tweets: []
-    }
-    var tweet_ids = []
-
-    var i = 1
-
-    var force_end = false
-
-
-    async function getTweets() {
-        let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-        let $ = cheerio.load(bodyHTML)
-        let all_tweets = $(bodyHTML).find('[data-testid="primaryColumn"] section > div > div > div')
-        let tweets = []
-        for (t of all_tweets) {
-            // don't allow duplicates
-            let id = Number($(t).attr('style').split(' ').at(1).replace('translateY(', '').replace('px);', ''))
-            if (!tweet_ids.includes(id) && tweets_obj.tweets.length + tweets.length < total_tweets) {
-                let text = $(t).find('[data-testid="tweetText"]').eq(0).text()
-                let date = $(t).find('time').attr('datetime')
-                let link = $(t).find('[data-testid="card.layoutLarge.media"] a').attr('href')
-
-
-                if (date !== undefined) {
-                    metadata = {
-                        handle,
-                        text,
-                        link,
-                        date,
-                        id
-                    }
-                    tweets.push(metadata)
-                    console.log(i)
-                    i++
-                }
-                else {
-                    if (!$(t).find('div > div > div').html()) {
-                        //console.log($(t).html())
-                        //force_end = true
-                    }
-                }
 
 
             }
-        }
-
-        return tweets
-    }
-    async function getTweetsAndScroll() {
-        //await waitForLoading('[role="progressbar"]')
-        if (!force_end) {
-            getTweets().then(async (tweets) => {
-                tweets.forEach((t) => {
-                    // what user sees
-                    tweets_obj.tweets.push({
-                        handle: t.handle,
-                        text: t.text,
-                        link: t.link,
-                        date: t.date
-                    })
-                    // add to exclusion array
-                    tweet_ids.push(t.id)
-                })
-                if (tweets_obj.tweets.length < total_tweets) {
-                    await scroll()
-                    await getTweetsAndScroll()
-                }
-                else {
-                    writeCsv()
-
-                }
-            })
-        }
-        else {
-            console.log('FORCE ENDED')
-            writeCsv()
-        }
-
-    }
-    async function scroll() {
-        var currentHTML = ''
-
+        })
+        let url = `https://twitter.com/search?q=from%3A${handle}%20since%3A2006-03-21&src=typed_query&f=live`
+        await page.goto(url, { waitUntil: 'networkidle2', timeout: 0 });
         await page.evaluate(() => {
-            document.querySelector('[data-testid="primaryColumn"] section > div > div > div:last-child').scrollIntoView()
-            currentHTML = document.querySelector('[data-testid="primaryColumn"] section > div > div').innerHTML
+            setInterval(() => {
+                window.scrollTo(0, document.body.scrollHeight);
+            }, 50)
         })
 
 
-        await page.waitForFunction(`document.querySelector('[data-testid="primaryColumn"] section > div > div').innerHTML != currentHTML`, { timeout: 5000 }).catch(() => { console.log('timeout') })
-        await page.waitForFunction(`Array.from(document.querySelectorAll('[data-testid="primaryColumn"] section > div > div > div')).map(e => Number(e.getAttribute('style').split(' ').at(1).replace('translateY(', '').replace('px);', ''))).includes(${tweet_ids.at(-1)})`, { timeout: 5000 }).catch(() => { console.log('timeout') })
+    })
+}
+handles_list.forEach((h, ii) => {
+    getTweetsFromHandle(h).then((all) => {
+        writeCsv(all, h).then(() => {
+            if (ii + 1 == handles_list.length) {
+                process.exit()
+            }
+        })
 
-    }
-    
-
-    function writeCsv() {
-        const csvWriter = createCsvWriter({
-            path: `${handle}.csv`,
-            header: [
-                { id: 'handle', title: 'handle' },
-                { id: 'text', title: 'text' },
-                { id: 'link', title: 'link' },
-                { id: 'date', title: 'date' },
-            ]
-        });
-        const data = tweets_obj.tweets
-        //console.log(data)
-        csvWriter
-            .writeRecords(data)
-            .then(() => console.log('The CSV file was written successfully'));
-    }
-    async function waitForLoading(css_selector) {
-        // wait for loader to appear then disapear
-        await page.waitForSelector(css_selector, { hidden: false });
-        console.log('started')
-        await page.waitForSelector(css_selector, { hidden: true });
-        console.log('ended')
-    }
+    })
+})
 
 
-    getTweetsAndScroll()
-*/
 
-})()
+async function writeCsv(data, h) {
+
+    var csvWriter = createCsvWriter({
+        path: `output/${h}.csv`,
+        header: [
+            { id: 'text', title: 'text' },
+            { id: 'date', title: 'date' }
+        ]
+    });
+    await csvWriter.writeRecords(data)
+}
